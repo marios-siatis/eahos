ed8dd4a2592cd5cfe6127b6203b1ad3bcc2f7167
Marios <marios.siati@outlook.com>
2026-08-09 11:02:26 +0100

    adds terraform state to s3

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = ">= 1.7.0"
 
+  backend "s3" {
+    key          = "eahos/production/terraform.tfstate"
+    region       = "eu-west-2"
+    encrypt      = true
+    use_lockfile = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
